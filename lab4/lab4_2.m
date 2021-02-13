09064b23e528ad01982f6d6690021c93c62e384e
RAJ GUPTA <201951123@iiitvadodara.ac.in>
2021-02-13 22:53:05 +0530

    Update lab4_2.m

--- lab4/lab4_2.m
+++ lab4/lab4_2.m
@@ -8,18 +8,24 @@ h = 10.^[-1:-1:-8];
 % Forward difference formula
 fwdDiff = (f(a + h) - f(a))./h;
 errFwd = abs(truVal - fwdDiff);
+disp(['Differentiation by forward difference is ', num2str(fwdDiff)]);
 disp(['Error in forward difference: ', num2str(errFwd)]);
 % Backward difference formula
 bckDiff = (f(a) - f(a - h))./h;
 errBck = abs(truVal - bckDiff);
+disp(['Differentiation by backward difference is ', num2str(bckDiff)]);
 disp(['Error in backward difference: ', num2str(errBck)]);
 % Central difference formula
 cntrDiff = (f(a + h) - f(a - h))./(2.*h);
 errCntr = abs(truVal - cntrDiff);
+disp(['Differentiation by central difference is ', num2str(cntrDiff)]);
 disp(['Error in central difference: ', num2str(errCntr)]);
 % Plots
 loglog(h, errFwd, '-b', h, errBck, '-g', h, errCntr, '-m');
 legend('Forward Error', 'Backward Error', 'Central Error', 'Location', 'northwest');
+ylabel('Error')
+xlabel('Step sizes')
+% Function
 function fx = f(x)
   fx = 2 - x + log(x);
 end
@@ -31,21 +37,13 @@ truVal = -1;
 h = 10.^[-1:-1:-8];
 % Forward difference formula for second order derivative
 fwdDiff = (f(a + 2*h) - 2*f(a + h) + f(a))./(h.*h);
-errFwd = abs(truVal - fwdDiff);
-disp(['Error in forward difference for second order derivative: ', num2str(errFwd)]);
+disp(['Differentiation by forward difference is ', num2str(fwdDiff)]);
 % Backward difference formula for second order derivative
 bckDiff = (f(a) - 2*f(a - h) + f(a - 2*h))./(h.*h);
-errBck = abs(truVal - bckDiff);
-disp(['Error in backward difference for second order derivative: ', num2str(errBck)]);
+disp(['Differentiation by backward difference is ', num2str(bckDiff)]);
 % Central difference formula for second order derivative
 cntrDiff = (f(a + h) - 2*f(a) + f(a - h))./(h.*h);
-errCntr = abs(truVal - cntrDiff);
-disp(['Error in central difference for second order derivative: ', num2str(errCntr)]);
-% Plots
-loglog(h, errFwd, '-b', h, errBck, '-g', h, errCntr, '-m');
-legend('Forward Error', 'Backward Error', 'Central Error', 'Location', 'northwest');
-ylabel('Error')
-xlabel('Step sizes')
+disp(['Differentiation by central difference is ', num2str(cntrDiff)]);
 % Function
 function fx = f(x)
   fx = 2 - x + log(x);
